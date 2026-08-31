56b75bc0bd4327e15d0364f17f15a91ab3cbdfac
Jigros <209211492+Jigros@users.noreply.github.com>
2026-08-31 08:41:22 +0200

    Remove unrelated r8168 workaround

--- nixos/r8168.nix
+++
@@ -1,7 +0,0 @@
-{config, ...}: {
-  # The old configuration used Realtek's r8168 driver because the in-tree
-  # r8169 driver intermittently dropped carrier on this RTL8168/8111 adapter.
-  boot.blacklistedKernelModules = ["r8169"];
-  boot.kernelModules = ["r8168"];
-  boot.extraModulePackages = [config.boot.kernelPackages.r8168];
-}
