e1c26de5396bc88a584e929080de8dfb8838dbee
Jigros <209211492+Jigros@users.noreply.github.com>
2026-09-16 21:06:10 +1000

    fix(spotatui): pin v0.42.0 for Spotify 429 handling

--- flake.nix
+++ flake.nix
@@ -56,6 +56,10 @@
       url = "github:anotherhadi/default-creds";
       flake = false;
     };
+    spotatui = {
+      url = "github:LargeModGames/spotatui/v0.42.0";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
 
     # Server
     nixarr.url = "github:nix-media-server/nixarr";
